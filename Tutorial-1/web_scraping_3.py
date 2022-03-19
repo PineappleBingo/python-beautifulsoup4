233aadcccff1cc522a4e1138c8f533915e230027
Jinho(David) Seo <brownkpt60@gmail.com>
2022-03-19 15:52:52 -0400

    updates

--- Tutorial-1/web_scraping_3.py
+++ Tutorial-1/web_scraping_3.py
@@ -7,6 +7,7 @@ doc = BeautifulSoup(result, "html.parser")
 
 tbody = doc.tbody
 trs = tbody.contents
+# print("len:", len(trs))
 
 # print("Parent:", trs[0].parent.name)
 # print(list(trs[0].next_siblings))
@@ -16,7 +17,12 @@ trs = tbody.contents
 # print((trs[0].contents))
 # print(list(trs[0].children))
 
-for tr in trs:
-    coin_name = tr.contents[0]
-    print(coin_name)
+prices = {}
+for tr in trs[:10]:
+    name, price = tr.contents[2:4]    
+    fixed_name = name.p.string
+    fixed_price = price.a.string
 
+    prices[fixed_name] = fixed_price
+
+print(prices)
