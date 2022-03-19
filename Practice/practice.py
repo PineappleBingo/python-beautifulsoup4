44d3fc5653048fe67d047c878576f05fe74f41f4
Jinho(David) Seo <brownkpt60@gmail.com>
2022-03-18 23:53:09 -0400

    updates

---
+++ Practice/practice.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.anntaylor.com/petite-dresses/cata000028"
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+# scrap title of title
+cat_title = doc.find("h1", class_="page-title")
+print(cat_title.string.strip())
+
+# scrap product lists
+products = doc.find_all("li", class_="product")
+# print("len:", len(products))
+# print(products)
+
+# find product name in the first element of proudct lists
+# product_name = products[0].find("strong").string.strip()
+# print(product_name)
+
+# find product link in the first element of proudct lists
+product_link = products[0].find("a", href=True)
+print(product_link["href"])
+
+# find each product's url and print
+for product in products:
+    url = product.find("a", href=True)
+    print(url["href"])
+
+# find each product's name and print
+for product in products:
+    print(product.find("strong").string.strip())
+
+
