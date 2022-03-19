74db577753ef5b210226e5a3cb603cd5f6382770
Jinho(David) Seo <brownkpt60@gmail.com>
2022-03-19 10:06:43 -0400

    updates

--- Tutorial-1/web_scraping_2.py
+++ Tutorial-1/web_scraping_2.py
@@ -1,4 +1,6 @@
+from distutils.ccompiler import new_compiler
 from bs4 import BeautifulSoup
+import re
 import requests
 
 url = "https://www.newegg.com/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Item=N82E16814932436&cm_sp=Homepage_TRENDINGNOW-_-P2_14-932-436-_-03182022"
@@ -14,4 +16,14 @@ prices = doc.find_all(text="$")
 parent = prices[0].parent
 # print(parent)
 strong = parent.find("strong")
-print(strong.string)
+# print(strong.string)
+
+# saving changes
+for price in prices:
+    tags = price.parent.find("strong")
+    # update price
+    tags.string = "0"
+    print(tags)
+
+# with open("web_scraped.html", "w") as file:
+#     file.write(str(doc))
