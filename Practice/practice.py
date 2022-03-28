89223638303ab4179ed9629380f62ba6e4913ba9
Jinho(David) Seo <brownkpt60@gmail.com>
2022-03-28 14:40:11 -0400

    updates

--- Practice/practice.py
+++ Practice/practice.py
@@ -19,23 +19,38 @@ products = doc.find_all("li", class_="product")
 # product_name = products[0].find("strong").string.strip()
 # print(product_name)
 
-# find product link in the first element of proudct lists
-product_link = products[0].find("a", href=True)
-print(product_link["href"])
+# # find product link in the first element of proudct lists
+# product_link = products[0].find("a", href=True)
+# print(product_link["href"])
 
-# find each product's url and print
-for product in products:
-    url = product.find("a", href=True)
-    print(url["href"])
+# # find each product's url and print
+# for product in products:
+#     url = product.find("a", href=True)
+#     print(url["href"])
 
-# find each product's name and print
-for product in products:
-    print(product.find("strong").string.strip())
+# # find each product's name and print
+# for product in products:
+#     print(product.find("strong").string.strip())
 
-items = dict()
+# # Select fist element of price
+# product_prices = products[0].select("span.price > span")
+# print("product_price:", product_prices)
 
-for product in products:
-    url = product.find("a", href=True)
-    name = product.find("strong").string.strip()
+# for product in products:
+#     product_price = product.select("span.price > span")[0].string
+#     # print(product_price[0].string)
+#     print(product_price)
 
-    print(url["href"], name)
+dresses = dict()
+for idx, product in enumerate(products):
+    url = product.select("div > a")[0].get("href")
+    name = product.select("strong")[0].string.strip()
+    price = product.select("span.price > span")[0].string
+
+    dresses[idx] = {
+                        "name" : name,
+                        "url"  : url,
+                        "price": price
+    }
+
+print(dresses[1])
